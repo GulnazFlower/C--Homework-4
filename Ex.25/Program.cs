c92deb3694d1bc046af4582f4927a02dc741efbe
GulnazBahir <gulnaz_flower@bk.ru>
2023-07-15 23:25:46 +0300

    Exercise 25

---
+++ Ex.25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetNaturalDegree(int a, int b)
+{   
+    int naturalDegree = 1;
+    while (count < b)
+    {
+        naturalDegree = naturalDegree * a;
+        count ++;
+    }
+    return naturalDegree;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = GetInput("Введите число A: ");
+int b = GetInput("Введите число B: ");
+Console.WriteLine($"Число {a} в степени {b} равно {GetNaturalDegree(a, b)}");
